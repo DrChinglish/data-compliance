32e3375e8b678fe700817ba26652a9ab3962224b
DrChinglish <42636453+DrChinglish@users.noreply.github.com>
2022-07-05 14:55:55 +0800

    Minor fix
    Deleted some useless folders

--- frontend/src/Pages/Detail.js
+++ frontend/src/Pages/Detail.js
@@ -53,7 +53,7 @@ export default class Detail extends Component {
                 </List>
             </Box>
         </Drawer>
-        <Box component='main' sx={{flexGrow:1,p:3}}>
+        <Box component='main' sx={{flexGrow:1,p:4}}>
             {/* main contents here */}
             <Toolbar/>
             <Typography paragraph>
@@ -61,16 +61,14 @@ export default class Detail extends Component {
                 tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
                 enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
                 imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                Csque felis imperdiet proin fermentum
                 leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
                 feugiat vivamus at augue. At augue eget arcu dictum varius duis at
                 consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
                 sapien faucibus et molestie ac.
             </Typography>
         </Box>
+        
     </Box>
     )   
   }
